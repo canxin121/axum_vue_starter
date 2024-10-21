323dff9715052f98bb09ee17ffc51d76dc9e0e72
canxin <69547456+canxin121@users.noreply.github.com>
2024-10-21 16:11:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# [Axum](https://github.com/tokio-rs/axum) + [Vue.js 3](https://vuejs.org/) + [Vite](https://vitejs.dev/) Template
+## Prerequisites:
+- [Rust](https://www.rust-lang.org/)
+- [Cargo Watch](https://crates.io/crates/cargo-watch) ```cargo install cargo-watch```
+- [pnpm](https://pnpm.io/)
+## Development
+- Install dependencies via ```pnpm install``` in /frontend/
+- Serve via ```pnpm dev``` in /frontend/
+## Build
+- Build via ```pnpm build``` in /frontend/
+
+![grafik](https://github.com/IllustrisJack/axum-vue-template/assets/76592751/8cf81597-f5fb-4b7f-8d35-3f419eabe0bc)
+
+
+
+
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import vueUrl from '../assets/vue.svg'
+const message = ref(null);
+
+async function getMessage() {
+    const response = await fetch('http://localhost:3030');
+    const json = await response.json();
+    message.value = json.message;
+}
+</script>
+
+<template>
+  <div style="container">
+    <h1 style="font-size:48px">
+      Rust &#129408;
+      + Vue <img :src="vueUrl" alt="vueLogo"/>
+      + Vite <img src=vite.svg alt="viteLogo"/>
+    </h1>
+  </div>
+  <p>Hello World!</p>
+  <div style="color:greenyellow" v-if="message">
+    Received: {{ message }}
+  </div>
+  <div style="color:orange" v-else>
+    No message
+  </div>
+  <button class="button" @click="getMessage()">GET MESSAGE</button>
+</template>
+
+<style scoped>
+.container {
+  display: inline-block;
+  align-items: center;
+  justify-content: center
+}
+
+.button {
+  margin: 20px 0 0 0;
+  font-weight: 700;
+  color: #0f0e1f;
+  background-color: white;
+}
+
+img {
+  width: 48px;
+}
+</style>
